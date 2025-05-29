3e30839f8d963afbf416917e49fe271d4824f76e
SheepEater <ponsuke1998@gmail.com>
2025-05-29 23:47:38 +0900

    デプロイチャレンジ２

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,14 +1,14 @@
 import './bootstrap';
 
 // 以下追加
-import '../css/app.css';
 import '../css/home.css';
 import '../css/components/post-card.css';
 import '../css/components/header.css';
 import '../css/components/search.css';
 
+import '../css/app.css';
+
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
+
